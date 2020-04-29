e441899fc1e26f0e15a5bfc461193cf54b60e36c
TALHIMOHAMMED <talhimohammed507@gmail.com>
2020-04-29 02:09:47 +0000

    home_profile_image

--- app/src/main/java/com/example/healthcare/home.java
+++ app/src/main/java/com/example/healthcare/home.java
@@ -1,16 +1,34 @@
 package com.example.healthcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class home extends AppCompatActivity {
 
     Button search  ;
     Button profile ;
+    CircleImageView profile_image ;
+    StorageReference storageReference ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +37,8 @@ public class home extends AppCompatActivity {
 
         search  = (Button)findViewById(R.id.search);
         profile =  (Button)findViewById(R.id.profile);
+        profile_image = (CircleImageView)findViewById(R.id.home_profile_image);
+
 
 
         search.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +54,33 @@ public class home extends AppCompatActivity {
                 startActivity(new Intent(v.getContext(),PatientProfile.class));
             }
         });
+
+        storageReference = FirebaseStorage.getInstance().getReference("Images").child(FirebaseAuth.getInstance().getCurrentUser().getUid()+".jpg");
+        final File localFile;
+        try {
+            localFile = File.createTempFile("images", "jpg");
+
+
+            storageReference.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+
+                            Bitmap myBitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            profile_image.setImageBitmap(myBitmap);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
